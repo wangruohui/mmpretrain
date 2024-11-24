205913f90eaa260c0976bb87e566bf9521b2c1df
wangruohui <12756472+wangruohui@users.noreply.github.com>
2024-11-24 23:17:11 +0800

    configs of huangfan 4 5 of convnext

---
+++ configs/huangfan/1120-convnext-v2-tiny_8xb64-fp16-dynamic_huangfan5_dedup-384px-large-crop.py
@@ -0,0 +1,51 @@
+_base_ = [
+    '../_base_/models/convnext_v2/tiny.py',
+    'huangfan5_dedup_bs64_swin_384_large_crop.py',
+    '../_base_/schedules/imagenet_bs1024_adamw_swin.py',
+    '../_base_/default_runtime.py',
+]
+
+# model setting
+model = dict(head=dict(num_classes=3))
+
+# dataset setting
+# train_dataloader = dict(batch_size=32)
+
+# schedule setting
+optim_wrapper = dict(
+    optimizer=dict(lr=3.2e-3/10),
+    clip_grad=None,
+)
+# schedule settings
+optim_wrapper = dict(type='AmpOptimWrapper', loss_scale='dynamic')
+
+# learning policy
+param_scheduler = [
+    # warm up learning rate scheduler
+    dict(
+        type='LinearLR',
+        start_factor=1e-3/10,
+        by_epoch=True,
+        end=40//2,
+        # update by iter
+        convert_to_iter_based=True),
+    # main learning rate scheduler
+    dict(type='CosineAnnealingLR', eta_min=1e-5, by_epoch=True, begin=40//2)
+]
+
+# train, val, test setting
+train_cfg = dict(by_epoch=True, max_epochs=300//4, val_interval=1)
+
+# runtime setting
+custom_hooks = [dict(type='EMAHook', momentum=1e-4, priority='ABOVE_NORMAL')]
+
+visualizer = dict(
+    type="UniversalVisualizer",
+    vis_backends=[dict(type="LocalVisBackend"), dict(type="WandbVisBackend")],
+)
+
+# configure default hooks
+default_hooks = dict(checkpoint=dict(interval=5))
+
+load_from = "checkpoints/convnext-v2-tiny_fcmae-in21k-pre_3rdparty_in1k-384px_20230104-d8579f84.pth"
+# load_from = "work_dirs/1103-convnext-v2-tiny_8xb48_huangfan4-noscale-384px-morewarm/epoch_40.pth"
